805853a10356609f2c66600ecd5cf789179add94
eshanized <m.eshanized@gmail.com>
2025-01-01 15:27:07 +0530

    feat: chnk name change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/you/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'script.js',
+        chunkFileNames: 'script.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'style.css';
+          }
+          return '[name][extname]';
+        },
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
